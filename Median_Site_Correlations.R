2efa8b6a986a67f8a4a7cc155951b20c8a495144
Vanessa Garayburu-Caruso <v.garayburucaruso@wsu.edu>
2024-04-05 16:21:38 -0700

    Update Median_Site_Correlations.R

--- Median_Site_Correlations.R
+++ Median_Site_Correlations.R
@@ -22,4 +22,57 @@ names(data)[2] = 'Treatment'
 # ==== Merging by Site and Treatment ====
 df = merge(om,data, by = c('Location','Treatment'))
 
-# ========= 
+# ========= Correlations ===========
+# Calculate ranks
+df$rank_Mean_OutliersRemoved_Respiration_Rate_mg_DO_per_kg_per_H <- rank(df$Mean_OutliersRemoved_Respiration_Rate_mg_DO_per_kg_per_H)
+df$rank_Gibbs_per_C <- rank(df$Gibbs_per_C)
+df$rank_Gibbs_per_compound <- rank(df$Gibbs_per_compound)
+df$rank_Lambda <- rank(df$Lambda)
+df$rank_ATP <- rank(df$Mean_ATP_picomol_per_g)
+
+# Calculate regression and add to data frame
+model <- lm(rank_Mean_OutliersRemoved_Respiration_Rate_mg_DO_per_kg_per_H ~ rank_Gibbs_per_C, data = df)
+df$predicted <- predict(model, df)
+
+p <- ggplot(df, aes(x = rank_Mean_OutliersRemoved_Respiration_Rate_mg_DO_per_kg_per_H, y = rank_Gibbs_per_C)) +
+  geom_point() +
+  stat_cor(method = "spearman",cor.coef.name = c( "rho"),
+           aes(label = paste(..r.label.., ..p.label.., sep = "~`,`~")), 
+           label.x = 17,label.y = 36,color='black',size=4)+ 
+  labs(title = " ",
+       x = "Rank of Mean_OutliersRemoved_Respiration_Rate_mg_DO_per_kg_per_H",
+       y = "Rank of Gibbs_per_C ") +
+  facet_wrap(~ Treatment)+ theme_bw()
+
+p1 <- ggplot(df, aes(x = rank_Mean_OutliersRemoved_Respiration_Rate_mg_DO_per_kg_per_H, y = rank_Gibbs_per_compound)) +
+  geom_point() +
+  stat_cor(method = "spearman",cor.coef.name = c( "rho"),
+           aes(label = paste(..r.label.., ..p.label.., sep = "~`,`~")), 
+           label.x = 17,label.y = 36,color='black',size=4)+ 
+  labs(title = " ",
+       x = "Rank of Mean_OutliersRemoved_Respiration_Rate_mg_DO_per_kg_per_H",
+       y = "Rank of Gibbs_per_Comp ") +
+  facet_wrap(~ Treatment)+ theme_bw()
+
+p2 <- ggplot(df, aes(x = rank_Mean_OutliersRemoved_Respiration_Rate_mg_DO_per_kg_per_H, y = rank_Lambda)) +
+  geom_point() +
+  stat_cor(method = "spearman",cor.coef.name = c( "rho"),
+           aes(label = paste(..r.label.., ..p.label.., sep = "~`,`~")), 
+           label.x = 17,label.y = 36,color='black',size=4)+ 
+  labs(title = " ",
+       x = "Rank of Mean_OutliersRemoved_Respiration_Rate_mg_DO_per_kg_per_H",
+       y = "Rank of Lambda ") +
+  facet_wrap(~ Treatment)+ theme_bw()
+
+
+p3 <- ggplot(df, aes(x = rank_Mean_OutliersRemoved_Respiration_Rate_mg_DO_per_kg_per_H, y = rank_ATP)) +
+  geom_point() +
+  stat_cor(method = "spearman",cor.coef.name = c( "rho"),
+           aes(label = paste(..r.label.., ..p.label.., sep = "~`,`~")), 
+           label.x = 17,label.y = 36,color='black',size=4)+ 
+  labs(title = " ",
+       x = "Rank of Mean_OutliersRemoved_Respiration_Rate_mg_DO_per_kg_per_H",
+       y = "Rank of ATP ") +
+  facet_wrap(~ Treatment)+ theme_bw()
+
+
