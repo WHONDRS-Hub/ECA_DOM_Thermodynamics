964a4c7fb0a0af58f209b8a30218b8efdceec9de
brieanne-forbes <brieanne.forbes@pnnl.gov>
2025-07-08 15:28:04 -0700

    Update column names to match other dps

--- Data/Format_CoreMS_Outputs.R
+++ Data/Format_CoreMS_Outputs.R
@@ -21,17 +21,22 @@ rm(list=ls(all=T))
 current_path <- rstudioapi::getActiveDocumentContext()$path 
 setwd(dirname(current_path))
 
-# ================================= get files ================================
+# ============================= update column names ============================
 
-data_files <- './Processed_clean_CoreMS_XML_Int_4_reps_1p5ppm_cal_Data.csv'%>%
+ './Processed_clean_CoreMS_XML_Int_4_reps_1p5ppm_cal_Data.csv'%>%
   read_csv() %>%
   rename_with(~ str_remove(.x, "_[^_]*$")) %>% # remove IAT from sample names
   rename(Calibrated_Mass = 1) %>%
   write_csv('./Processed_clean_CoreMS_XML_Int_4_reps_1p5ppm_cal_Data.csv')
 
-# write_csv(data, i)
+'./FTICR_combined_unique_formulas_Data.csv'%>%
+  read_csv() %>%
+  rename_with(~ str_remove(.x, "_[^_]*$")) %>% # remove IAT from sample names
+  rename(Calibrated_Mass = 1) %>%
+  write_csv('./FTICR_combined_unique_formulas_Data.csv')
 
-mol_files <- './Processed_clean_CoreMS_XML_Int_1p5ppm_cal_pts_Mol.csv' %>%
+
+'./Processed_clean_CoreMS_XML_Int_1p5ppm_cal_pts_Mol.csv' %>%
   read_csv() %>%
   select(-`Molecular.Formula`) %>% #remove column as it is the same as MolForm column 
   rename(Calibrated_Mass = 1,
